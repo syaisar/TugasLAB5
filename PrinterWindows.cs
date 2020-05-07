1352f5f5f6d4296574ec86f7dbb4c155b7b22fb7
syaisar toumahu <61915350+syaisar@users.noreply.github.com>
2020-05-07 10:42:45 +0700

    Add files via upload

---
+++ PrinterWindows.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class PrinterWindows
+    {
+        public string ukuran { get; set; }
+        public string ketrngan { get; set; }
+
+        public virtual void show()
+        {
+            Console.WriteLine("Display dimension : ");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer printing ...");
+        }
+    }
+    public class Epson : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("Epson Display dimension : 10*11");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing ...");
+        }
+    }
+    public class Canon : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("Canon display : 9.5*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing ...");
+        }
+    }
+    public class Laserjet : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("Laserjet display dimension : 12*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Laserjet printer printing ...");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih Printer");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. Laserjet\n");
+
+            Console.Write("Nomer Printer [1,2,3] : ");
+            int noprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (noprinter == 1)
+                printer = new Epson();
+            else if (noprinter == 2)
+                printer = new Canon();
+            else
+                printer = new Laserjet();
+
+            printer.show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
